8fb7568ce1c94eeef089a19b5e253ceac77c703a
johncolvin-og <46972520+johncolvin-og@users.noreply.github.com>
2021-09-18 13:16:37 -0400

    add src files

---
+++ src/chat_client.py
@@ -0,0 +1,74 @@
+import logging
+import socket
+import argparse
+import sys
+
+
+def _get_arg_parser():
+    parser = argparse.ArgumentParser(description='Start simple chat client')
+    parser.add_argument(
+        '--port',
+        metavar='port',
+        type=int,
+        nargs='?',
+        help='The port to which to connect',
+        default=5678)
+
+    parser.add_argument(
+        '--host',
+        metavar='host',
+        type=str,
+        nargs='?',
+        help='The host to which to connect',
+        default='localhost')
+
+    parser.add_argument(
+        '--verbosity',
+        metavar='verbosity',
+        type=int,
+        nargs='?',
+        choices=[logging.DEBUG, logging.INFO, logging.WARN, logging.ERROR],
+        help='The verbosity level',
+        default=logging.INFO)
+    return parser
+
+
+def _init_logger(verbosity):
+    logger = logging.getLogger()
+    logger.setLevel(verbosity)
+    # print logs to console
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    return logger
+
+def main():
+    parser = _get_arg_parser()
+    args = parser.parse_args()
+    host = args.host
+    port = args.port
+    buffer_size = 1024
+
+    logger = _init_logger(args.verbosity)
+
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    logger.debug(f'connecting to {host}:{port}')
+    conn.connect((host, port))
+    logger.debug(f'connecting to {host}:{port} DONE')
+
+    def prompt_input():
+        return input('Send msg : ')
+
+    msg_out = prompt_input()
+    while msg_out.lower() != 'exit' and msg_out.lower() != 'quit':
+        conn.sendall(bytes(msg_out, 'utf-8'))
+        msg_in = conn.recv(buffer_size)
+        logger.info(
+            f'''Received msg: {msg_in.decode('utf-8')}\
+        \n\tlocal_addr   : {conn.getsockname()}\
+        \n\tforeign_addr : {conn.getpeername()}''')
+        msg_out = prompt_input()
+    # Tell the server that we are disconnecting (so it doesn't think the connection dropped due to an error)
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/chat_server.py
@@ -0,0 +1,74 @@
+import logging
+import socket
+import argparse
+import sys
+
+
+def _get_arg_parser():
+    parser = argparse.ArgumentParser(description='Start simple chat server')
+    parser.add_argument(
+        '--port',
+        metavar='port',
+        type=int,
+        nargs='?',
+        help='The port to which to connect',
+        default=5678)
+
+    parser.add_argument(
+        '--host',
+        metavar='host',
+        type=str,
+        nargs='?',
+        help='The host to which to connect',
+        default='localhost')
+
+    parser.add_argument(
+        '--verbosity',
+        metavar='verbosity',
+        type=int,
+        nargs='?',
+        choices=[logging.DEBUG, logging.INFO, logging.WARN, logging.ERROR],
+        help='The verbosity level',
+        default=logging.INFO)
+    return parser
+
+
+def _init_logger(verbosity):
+    logger = logging.getLogger()
+    logger.setLevel(verbosity)
+    # print logs to console
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    return logger
+
+
+def main():
+    parser = _get_arg_parser()
+    args = parser.parse_args()
+    host = args.host
+    port = args.port
+    buffer_size = 1024
+
+    logger = _init_logger(args.verbosity)
+
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # reserve port for this socket (once 'bind' is called, other sockets won't be allowed to use this port)
+    conn.bind((host, port))
+    # put socket in 'listen' mode
+    conn.listen(1)
+    # accept incoming connection (i.e., from the client).  This will block until the connection is made
+    (conn_in, addr_in) = conn.accept()
+
+    # receive msg from client.  This will block until a message is received
+    msg_in = conn_in.recv(buffer_size)
+    while msg_in:
+        msg_in_str = msg_in.decode('utf-8')
+        logger.info(
+            f'''Received msg: {msg_in_str}\
+        \n\tlocal_addr   : {conn_in.getsockname()}\
+        \n\tforeign_addr : {addr_in}''')
+        conn_in.sendall(f'{msg_in_str} - copy that'.encode('utf-8'))
+        msg_in = conn_in.recv(buffer_size)
+
+
+if __name__ == '__main__':
+    main()
